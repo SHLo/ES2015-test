749f275035e52e1f66623884de7ace64d0d84fda
shlo <shlo.sam@gmail.com>
2016-06-15 16:57:54 +0800

    react hello world

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,14 @@
 import '../index.html';
 import '../css/styles.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import msg from './msg.js';
 document.querySelector('h1').textContent = `${msg.msg} 1`;
 document.querySelector('h1').classList.add('jumbotron');
+
+ReactDOM.render(
+    <h1>Hello!</h1>,
+    document.getElementById('example')
+);
--- src/js/msg.js
+++ src/js/msg.js
@@ -1,4 +1,4 @@
 module.exports = {
-  msg: 'aaajkaaaaahh:ia',
+  msg: 'kkiaaajkaaaaahh:ia',
 };
 
