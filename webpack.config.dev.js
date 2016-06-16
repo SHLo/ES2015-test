c914f81ca984f59721c69668ad975fd39d3f32ef
shlo <shlo.sam@gmail.com>
2016-06-16 17:05:01 +0800

    add open browser plugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,14 +1,23 @@
 var base = require('./webpack.config.base');
 var webpack = require('webpack');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 var dev = base;
+dev.devServerPort = 3000;
 dev.devtool = 'cheap-eval-source-map';
 dev.entry = [
-    'webpack-dev-server/client?http://localhost:3000',
+    'webpack-dev-server/client?http://localhost:' + dev.devServerPort,
     'webpack/hot/dev-server',
 ]
     .concat(dev.entry);
-dev.plugins.push(new webpack.HotModuleReplacementPlugin());
-dev.devServerPort = 3000;
+
+dev.plugins = dev.plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({
+        url: 'http://localhost:' + dev.devServerPort,
+        ignoreErrors: true,
+        browser: 'Google Chrome'
+    }),
+]);
 
 module.exports = dev;
