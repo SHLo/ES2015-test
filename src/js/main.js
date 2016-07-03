b72b87c32bd074125bdbd1a8ef99b1603931be6b
shlo <shlo.sam@gmail.com>
2016-07-03 20:00:12 +0800

    use react-css-modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import '../index.html';
-// import styles from '../css/styles.css';
+import cssModules from 'react-css-modules';
+import styles from '../css/styles.css';
 // import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -8,7 +9,10 @@ import msg from './msg.js';
 document.querySelector('h1').textContent = `${msg.msg} 1`;
 document.querySelector('h1').classList.add('jumbotron');
 
+let Gg = () => <a className="btn btn-success" styleName="test">Hello!</a>;
+Gg = cssModules(Gg, styles);
+
 ReactDOM.render(
-  <a className="btn btn-default">Hello!</a>,
+  <Gg />,
   document.getElementById('example')
 );
